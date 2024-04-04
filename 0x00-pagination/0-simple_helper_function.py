49ede5c3dd0c7b4a576cbee4c5fab07b930fc686
evie-8 <neouma29@gmail.com>
2024-04-04 17:51:18 +0300

    corrections

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,10 +5,8 @@ Module that returns the starting index  and ending index
 from typing import Tuple
 
 
-def index_range(page: int, page_size:int) -> Tuple[int, int]:
-    '''
-    Computes index range
-    '''
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''Computes index range'''
     start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
