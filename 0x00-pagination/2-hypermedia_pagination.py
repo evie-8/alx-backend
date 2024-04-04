0f6a72972e21bf4f6b3976e2a27f886b0be919f2
evie-8 <neouma29@gmail.com>
2024-04-04 19:22:05 +0300

    task 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -2,7 +2,7 @@
 '''get data on specific page'''
 import csv
 import math
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -42,18 +42,19 @@ class Server:
             return []
         return pageData[start:end]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        """Gets Info about a page"""
-        assert type(page) == int and type(page_size) == int
-        assert (page > 0 and page_size > 0)
-        pageData = self.get_page(page, page_size)
-        start, end = index_range(page, page_size)
-        totalPages = math.ceil(len(self.dataset()) / page_size)
-        pageInfo = {
-                'page_size': len(page_data),
-                'page': page,
-                'next_page': page + 1 if totalPages else None,
-                'prev_page': page - 1 if start > 0 else None,
-                'total_pages': totalPages
-                }
-        return pageInfo
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        '''retrieves a info about a page
+        '''
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+        page_data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        page_info = {
+            'page_size': page,
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if page + 1 < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_page': total_pages,
+        }
+        return page_info
