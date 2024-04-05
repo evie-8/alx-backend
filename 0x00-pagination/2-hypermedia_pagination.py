a76a2565c322021d5cce934cd0866e18be5e8b8e
evie-8 <neouma29@gmail.com>
2024-04-05 10:05:22 +0300

    corrections

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -43,19 +43,17 @@ class Server:
         return pageData[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        '''retrieves a info about a page
-        '''
-        assert type(page) == int and type(page_size) == int
-        assert type(page > 0 and page_size > 0)
-
+        """Retrieves information about a page.
+        """
         page_data = self.get_page(page, page_size)
-        total_pages = math.ceil(len(self.dataset()) / page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
         page_info = {
             'page_size': len(page_data),
             'page': page,
             'data': page_data,
-            'next_page': page + 1 if page + 1 < total_pages else None,
-            'prev_page': page - 1 if page > 1 else None,
-            'total_page': total_pages,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages,
         }
         return page_info
