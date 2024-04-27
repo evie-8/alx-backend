fddbe5158307ccdd5481f3d71b9b9716b0e4b733
evie-8 <neouma29@gmail.com>
2024-04-27 14:56:09 +0300

    new

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -5,6 +5,8 @@ Module for task 3
 
 from flask_babel import Babel
 from flask import Flask, render_template, request
+from typing import Union
+
 
 app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
@@ -22,6 +24,14 @@ class Config(object):
 app.config.from_object(Config)
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
 @babel.localeselector
 def get_locale() -> str:
     """get locale"""
@@ -31,10 +41,30 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+def get_user() -> Union[dict, None]:
+    '''
+        Get user from session as per variable.
+    '''
+    try:
+        login_as = request.args.get('login_as', None)
+        user = users[int(login_as)]
+    except Exception:
+        user = None
+
+
+@app.before_request
+def before_request():
+    '''
+        Operations before request.
+    '''
+    user = get_user()
+    g.user = user
+
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def helloWorld() -> str:
     """task 4 route"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
