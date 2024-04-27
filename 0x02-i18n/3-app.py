85181f0740dd39a62447524ffd50c5e2a98ed9bc
evie-8 <neouma29@gmail.com>
2024-04-27 14:20:55 +0300

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Module for task 2
+"""
+
+from flask_babel import Babel
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+
+
+class Config(object):
+    '''
+        Babel configuration.
+    '''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def helloWorld() -> str:
+    """task 2 route"""
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
